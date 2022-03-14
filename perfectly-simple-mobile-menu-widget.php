eb9edbbdc50ab6016cbd107aaa886656bbe0627d
Nathan Eckberg <nathan.eckberg@gmail.com>
2022-03-14 13:21:38 -0500

    add license, readme, and main plugin file

---
+++ perfectly-simple-mobile-menu-widget.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://nathaneckberg.com/perfectly-simple-mobile-menu-widget
+ * @since             1.0.0
+ * @package           PSMMW
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Perfectly Simple Mobile Menu Widget
+ * Plugin URI:        TODO: <link to github repo>
+ * Description:       Simple slide-in mobile hamburger menu with widget area
+ * Version:           1.0.0
+ * Author:            Nathan Eckberg
+ * Author URI:        https://nathaneckberg.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       psmmw
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
