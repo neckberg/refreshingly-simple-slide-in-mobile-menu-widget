dd7304c76ae6c4a53489780376a50fbc88fbed90
Nathan Eckberg <nathan.eckberg@gmail.com>
2022-03-15 16:33:22 -0500

    ran composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '16dde8255f280f9678a05de1e7cc0bfb64514c96',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '16dde8255f280f9678a05de1e7cc0bfb64514c96',
+            'dev_requirement' => false,
+        ),
+    ),
+);
