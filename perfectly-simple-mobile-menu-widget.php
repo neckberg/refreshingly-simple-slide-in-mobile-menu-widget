e0e9985ba14b39c2c604287c04958914d6986df3
Nathan Eckberg <nathan.eckberg@gmail.com>
2022-03-16 15:58:22 -0500

    move everything into the class

--- perfectly-simple-mobile-menu-widget.php
+++ perfectly-simple-mobile-menu-widget.php
@@ -32,36 +32,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
-// $setup = new PSMMW\Setup();
-$setup = PSMMW\Setup::run();
-
-add_action('wp_enqueue_scripts', 'psmmw_load_scripts_and_styles');
-function psmmw_load_scripts_and_styles() {
-  wp_enqueue_script('psmmw', plugin_dir_url( __FILE__ ) . '/scripts/dist/psmmw.js');
-	wp_localize_script('psmmw', 'pw_script_vars', array(
-			// 'alert' => __('Hey! You have clicked the buttons!', 'psmmw'),
-      'ajax_url' => site_url( 'wp-json/psmmw/v1/html' ),
-		)
-	);
-  wp_enqueue_style('psmmw', plugin_dir_url( __FILE__ ) . '/styles/dist/psmmw.css');
-}
-
-register_sidebar([
-  'name'          => 'PSMMW Mobile Drawer',
-  'id'            => 'psmmw-mobile-drawer',
-  'before_widget' => '<div class="widget">',
-  'after_widget'  => '</div>',
-  'before_title'  => '<h3>',
-  'after_title'   => '</h3>',
-]);
-
-add_action( 'rest_api_init', function () {
-  register_rest_route( 'psmmw/v1', '/html', array(
-    'methods' => 'GET',
-    'callback' => 'psmmw_html',
-  ) );
-} );
-
-function psmmw_html($parameters) {
-  return PSMMW\Setup::psmmw_html();
-}
+$setup = PSMMW\Setup::run( plugin_dir_url( __FILE__ ) );
--- src/Setup.php
+++ src/Setup.php
@@ -1,60 +1,86 @@
 <?php
 namespace PSMMW;
-// error_log(__FILE__);
 
 class Setup {
-   private function __construct() {}
-   public static function run() {
-   }
-   public static function psmmw_html() {
-     return [
-       // 'html' => psmmw_html_container(),
-       'hamburger' => self::psmmw_html_hamburger(),
-       'backdrop' => self::psmmw_html_backdrop(),
-       'drawer' => self::psmmw_html_mobile_drawer(),
-     ];
-   }
-   private static function psmmw_html_container() {
-     ob_start();
+  public static $plugin_root_url;
+  private function __construct() {}
+  public static function run($plugin_root_url) {
+    self::$plugin_root_url = $plugin_root_url;
+    add_action('wp_enqueue_scripts', 'PSMMW\Setup::psmmw_load_scripts_and_styles');
+
+    register_sidebar([
+      'name'          => 'PSMMW Mobile Drawer',
+      'id'            => 'psmmw-mobile-drawer',
+      'before_widget' => '<div class="widget">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>',
+    ]);
+
+    add_action( 'rest_api_init', function () {
+      register_rest_route( 'psmmw/v1', '/html', array(
+        'methods' => 'GET',
+        'callback' => 'PSMMW\Setup::psmmw_html',
+      ) );
+    } );
+  }
+  public static function psmmw_load_scripts_and_styles() {
+    wp_enqueue_script('psmmw', self::$plugin_root_url . '/scripts/dist/psmmw.js');
+    wp_localize_script('psmmw', 'pw_script_vars', array(
+      // 'alert' => __('Hey! You have clicked the buttons!', 'psmmw'),
+      'ajax_url' => site_url( 'wp-json/psmmw/v1/html' ),
+      )
+    );
+    wp_enqueue_style('psmmw', self::$plugin_root_url . '/styles/dist/psmmw.css');
+  }
+  public static function psmmw_html() {
+   return [
+     // 'html' => psmmw_html_container(),
+     'hamburger' => self::psmmw_html_hamburger(),
+     'backdrop' => self::psmmw_html_backdrop(),
+     'drawer' => self::psmmw_html_mobile_drawer(),
+   ];
+  }
+  private static function psmmw_html_container() {
+   ob_start();
+   ?>
+   <div aria-hidden="true" class="psmmw-container">
+     <?php
+     echo psmmw_html_hamburger();
+     echo psmmw_html_backdrop();
+     echo psmmw_html_mobile_drawer();
      ?>
-     <div aria-hidden="true" class="psmmw-container">
+   </div>
+   <?php
+   return ob_get_clean();
+  }
+  private static function psmmw_html_hamburger() {
+   ob_start();
+   ?>
+   <div aria-hidden="true" class="psmmw-hamburger-bar">
+     <span class="psmmw-icon dashicons dashicons-menu"></span>
+   </div>
+   <?php
+   return ob_get_clean();
+  }
+  private static function psmmw_html_backdrop() {
+   ob_start();
+   ?><div aria-hidden="true" class="psmmw-backdrop"></div><?php
+   return ob_get_clean();
+  }
+  private static function psmmw_html_mobile_drawer() {
+   if ( !is_active_sidebar('psmmw-mobile-drawer') ) return;
+   ob_start();
+   ?>
+   <aside aria-hidden="true" class="psmmw-mobile-drawer">
+     <span class="psmmw-drawer-close psmmw-icon dashicons dashicons-no"></span>
+       <div class="widgets">
        <?php
-       echo psmmw_html_hamburger();
-       echo psmmw_html_backdrop();
-       echo psmmw_html_mobile_drawer();
+       dynamic_sidebar('psmmw-mobile-drawer');
        ?>
      </div>
-     <?php
-     return ob_get_clean();
-   }
-   private static function psmmw_html_hamburger() {
-     ob_start();
-     ?>
-     <div aria-hidden="true" class="psmmw-hamburger-bar">
-       <span class="psmmw-icon dashicons dashicons-menu"></span>
-     </div>
-     <?php
-     return ob_get_clean();
-   }
-   private static function psmmw_html_backdrop() {
-     ob_start();
-     ?><div aria-hidden="true" class="psmmw-backdrop"></div><?php
-     return ob_get_clean();
-   }
-   private static function psmmw_html_mobile_drawer() {
-     if ( !is_active_sidebar('psmmw-mobile-drawer') ) return;
-     ob_start();
-     ?>
-     <aside aria-hidden="true" class="psmmw-mobile-drawer">
-       <span class="psmmw-drawer-close psmmw-icon dashicons dashicons-no"></span>
-         <div class="widgets">
-         <?php
-         dynamic_sidebar('psmmw-mobile-drawer');
-         ?>
-       </div>
-     </aside>
-     <?php
-     return ob_get_clean();
-   }
-
+   </aside>
+   <?php
+   return ob_get_clean();
+  }
 }
